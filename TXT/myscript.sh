ecb8358311f70b278615f149e7d5a16bba533c41
Reiou Nagata <sherlocknagata@gmail.com>
2022-09-17 19:05:19 +0700

    Hopefully this time is right

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -12,7 +12,7 @@
 # You new to set "REC2" with your own Public-Key Identity!
 # Check it out with "gpg --list-key"
 # ####################### Replace REC2 ####
-REC2="60914D29C01C81F1"
+REC2="740338CB74AB31809A1803D885485AE30093A9BF"
 # ####################### ####### #### ####
 # REC1: public key
 REC1="63FB12B215403B20"
